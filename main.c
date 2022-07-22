528478197b958775f159525df53635985f00b616
Thomzzz <thomazvanbeeck@gmail.com>
2022-07-22 21:55:47 +0200

    Set pin dirs and add pullup

--- main.c
+++ main.c
@@ -5,27 +5,19 @@
 
 int main() {
 
-    static const uint8_t debouncePin = 25;
-    static const uint8_t debugPin = 26;
+    static const uint debouncePin = 6;
+    static const uint debugPin = 7;
+    static const float frequency = 2000.0f;
 
-    // Choose PIO instance (0 or 1)
     PIO pio = pio0;
-
-    // Get first free state machine in PIO 0
-    uint sm = pio_claim_unused_sm(pio, true);
-
-    // Add PIO program to PIO instruction memory. SDK will find location and
-    // return with the memory offset of the program.
+    uint sm = 0;
     uint offset = pio_add_program(pio, &debounce_program);
 
-    // Initialize the program using the helper function in our .pio file
-    debounce_program_init(pio, sm, offset, 10000.0, debouncePin, debugPin);
-
-    // Start running our PIO program in the state machine
+    debounce_program_init(pio, sm, offset, debouncePin, debugPin, frequency);
     pio_sm_set_enabled(pio, sm, true);
 
     // Set debounce (10ms?)
-    debounce_program_set_debounce(pio, sm, 50);
+    //debounce_program_set_debounce(pio, sm, 50);
 
     // Do nothing
     while (true) 
