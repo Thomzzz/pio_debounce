bc5e980441ad84da7549f1030cdc26b4b8e4a5e1
Thomzzz <thomazvanbeeck@gmail.com>
2022-07-22 23:49:23 +0200

    Debounce time can be set in milliseconds instead of an arbitrary value

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main() {
     pio_sm_set_enabled(pio, sm, true);
 
     // Set debounce (10ms)
-    debounce_program_set_debounce(pio, sm, 625000);
+    debounce_program_set_debounce(pio, sm, 10.0f);
 
     // Do nothing
     while (true) 
