cc9fda61e3eef56614920f298872336817d9d5b1
Thomzzz <thomazvanbeeck@gmail.com>
2022-07-22 23:11:24 +0200

    Force user to set the debounce period

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main() {
 
     static const uint debouncePin = 6;
     static const uint debugPin = 7;
-    static const float frequency = 2000.0f;
+    static const float frequency = 125000000.0f;
 
     PIO pio = pio0;
     uint sm = 0;
@@ -16,8 +16,8 @@ int main() {
     debounce_program_init(pio, sm, offset, debouncePin, debugPin, frequency);
     pio_sm_set_enabled(pio, sm, true);
 
-    // Set debounce (10ms?)
-    //debounce_program_set_debounce(pio, sm, 50);
+    // Set debounce (10ms)
+    debounce_program_set_debounce(pio, sm, 625000);
 
     // Do nothing
     while (true) 
